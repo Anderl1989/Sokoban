504a2c955519d97fe23dfdb2b1f7171bfcfff791
Andreas Kasch <ak@presono.com>
2022-07-08 07:24:03 +0200

    drawing the playfield

--- src/game.js
+++ src/game.js
@@ -60,6 +60,62 @@ class Sokoban {
 
             this.level.push(levelRowData);
         }
+
+        if (!this.player) {
+            throw new Error('No player found!');
+        }
+
+        this.drawPlayfield();
+    }
+
+    drawPlayfield() {
+        const playfield = document.getElementById('playfield');
+
+        for (let y = 0; y < this.level.length; y++) {
+            const row = this.level[y];
+            for (let x = 0; x < row.length; x++) {
+                const char = row[x];
+                const img = document.createElement('img');
+                img.style.left = `${x * 48}px`;
+                img.style.top = `${y * 48}px`;
+                switch (char) {
+                    case WALL:
+                        img.src = 'assets/flat/wall.png';
+                        img.alt = 'Wall';
+                        break;
+                    case TARGET:
+                        img.src = 'assets/flat/target.png';
+                        img.alt = 'Target';
+                        break;
+                    case FLOOR_INSIDE:
+                        img.src = 'assets/flat/floor.png';
+                        img.alt = 'Floor';
+                        break;
+                    case FLOOR_OUTSIDE:
+                        img.src = 'assets/flat/grass.png';
+                        img.alt = 'Grass';
+                        break;
+                }
+                playfield.appendChild(img);
+            }
+        }
+
+        const playerImg = document.createElement('img');
+        playerImg.style.left = `${this.player.x * 48}px`;
+        playerImg.style.top = `${this.player.y * 48}px`;
+        playerImg.src = 'assets/flat/player.png';
+        playerImg.alt = 'Player';
+        playfield.appendChild(playerImg);
+
+        for (let i = 0; i < this.boxes.length; i++) {
+            const box = this.boxes[i];
+            const boxImg = document.createElement('img');
+            boxImg.style.left = `${box.x * 48}px`;
+            boxImg.style.top = `${box.y * 48}px`;
+            boxImg.src = 'assets/flat/box.png';
+            boxImg.alt = 'Box';
+            playfield.appendChild(boxImg);
+        }
     }
 }
 
